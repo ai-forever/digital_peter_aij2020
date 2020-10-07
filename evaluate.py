09447491ddfc94c42e2af8df0c6a9a6b63b4dea1
mike0sv <mike0sv@gmail.com>
2020-10-07 16:14:47 +0300

    scoring fixes and submit example

--- evaluate.py
+++ evaluate.py
@@ -1,28 +1,36 @@
+import os
 import sys
 import editdistance
 
-#Это скрипт который посчитает 3 метрики качества, по которым оценивается решение
+
+def read(path, names=None):
+    if names is None:
+        names = os.listdir(path)
+    data = []
+    for name in names:
+        with open(os.path.join(path, name), 'r') as file:
+            data.append(file.read().strip())
+
+    return names, data
+
+
+# Это скрипт который посчитает 3 метрики качества, по которым оценивается решение
 def evaluate():
-    #Аргументы - названия файлов с предсказаниями и правильными ответами.
-    #В true записаны построчно правильные ответы. В pred - соответствующие предсказания
+    # Аргументы - названия файлов с предсказаниями и правильными ответами.
+    # В true записаны построчно правильные ответы. В pred - соответствующие предсказания
     pred_path = sys.argv[1]
     true_path = sys.argv[2]
 
+    names, true_text = read(true_path)
 
-    with open(pred_path) as f:
-        prediction = f.readlines()
-    predictions = [x.rstrip('\n') for x in prediction] 
+    _, predictions = read(pred_path, names)
 
-    with open(true_path) as f:
-        true_text = f.readlines()
-
-    true_text = [x.rstrip('\n') for x in true_text] 
-
-    #По сути, мы в цикле пробегаемся по предсказаниям, считаем расстояние Левенштейна, а затем делим сумму расстояний на сумму длин всех правильных ответов
+    # По сути, мы в цикле пробегаемся по предсказаниям, считаем расстояние Левенштейна,
+    # а затем делим сумму расстояний на сумму длин всех правильных ответов
     def cer():
         numCharErr = 0
         numCharTotal = 0
-        
+
         for i in range(len(predictions)):
             pred = predictions[i]
             true = true_text[i]
@@ -30,9 +38,10 @@ def evaluate():
             numCharErr += dist
             numCharTotal += len(true)
         charErrorRate = numCharErr / numCharTotal
-        return charErrorRate*100
-        
-    #Аналогичный подход, как и для CER. Только объектом является слово, а не символ. Соответственно в подсчете расстояния участвует два массива - pred и true, в массивах содержатся слова
+        return charErrorRate * 100
+
+    # Аналогичный подход, как и для CER. Только объектом является слово, а не символ.
+    # Соответственно в подсчете расстояния участвует два массива - pred и true, в массивах содержатся слова
     def wer():
         word_eds, word_true_lens = [], []
         for i in range(len(predictions)):
@@ -43,11 +52,12 @@ def evaluate():
             true_words = true.split()
             word_eds.append(editdistance.eval(pred_words, true_words))
             word_true_lens.append(len(true_words))
-    
+
         wordErrorRate = sum(word_eds) / sum(word_true_lens)
-        return wordErrorRate*100
-        
-    #Наиболее простая метрика, которая считает количество полных совпадений предложений (pred и true) и делит его на общий размер теста
+        return wordErrorRate * 100
+
+    # Наиболее простая метрика, которая считает количество полных совпадений предложений (pred и true)
+    # и делит его на общий размер теста
     def string_acc():
         numStringOK = 0
         numStringTotal = 0
@@ -58,18 +68,17 @@ def evaluate():
 
             numStringOK += 1 if true == pred else 0
             numStringTotal += 1
-        
+
         stringAccuracy = numStringOK / numStringTotal
-        
-        return stringAccuracy*100
-    
-    
-    #Вызов функций и подсчет метрик
+
+        return stringAccuracy * 100
+
+    # Вызов функций и подсчет метрик
     charErrorRate = cer()
     wordErrorRate = wer()
     stringAccuracy = string_acc()
-    
-    #Выведем построчно правильные ответы и предсказания
+
+    # Выведем построчно правильные ответы и предсказания
     print('Ground truth -> Recognized')
     for i in range(len(predictions)):
         pred = predictions[i]
@@ -77,12 +86,12 @@ def evaluate():
 
         dist = editdistance.eval(pred, true)
 
-        print('[OK]' if dist==0 else '[ERR:%d]' % dist,'"' + true + '"', '->', '"' + pred + '"')
-    
+        print('[OK]' if dist == 0 else '[ERR:%d]' % dist, '"' + true + '"', '->', '"' + pred + '"')
+
     print('Character error rate: %f%%' % charErrorRate)
     print('Word error rate: %f%%' % wordErrorRate)
     print('String accuracy: %f%%' % stringAccuracy)
-    
+
 
 if __name__ == "__main__":
     evaluate()
